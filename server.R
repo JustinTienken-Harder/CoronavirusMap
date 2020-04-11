de5201de3913d78189fe1cf975f4b5c8a9c53ec8
JustinHarder <justin.tienken-har13@ncf.edu>
2020-04-11 13:36:08 -0400

    Massive Update To the Website

--- server.R
+++ server.R
@@ -4,48 +4,233 @@ library(ggplot2)
 library(readr)
 library(tidyverse)
 library(maps)
+#install.packages("mapproj")
+library(mapproj)
+library(plotly)
+library(DT)
 
 shinyServer(function(input, output, session) {
+  #output$menu <- renderMenu({
+    
+  #})
   
-  df <- reactiveFileReader(
+  
+  county_data <- reactiveFileReader(
+    intervalMillis = 10000,
+    session = session,
+    filePath = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv",
+    readFunc = read_csv
+  )
+  
+  
+  state_data <- reactiveFileReader(
           intervalMillis = 10000, 
           session = session,
-          filePath = "https://raw.githubusercontent.com/bklingen/DataViz2020_Maps/master/CoronaCases.csv",
+          filePath = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv",
           readFunc = read_csv)
   
-  output$mydata <-renderTable({
-    corona <- df()
-    corona.cases <- corona %>% count(County) %>% mutate(County = tolower(County))
-    corona.cases <- corona.cases %>% mutate(County = fct_recode(County, 
-                                                                `miami-dade` = "dade", 
-                                                                `st johns` = "st. johns", 
-                                                                `st lucie` = "st. lucie")) %>% 
-      filter(County != "unknown") %>% arrange(desc(n), County)
+  output$usadata <-renderDT({
+    
+    state_data() %>% 
+      arrange(desc(cases), state) %>% 
+      mutate(cases = as.integer(cases), deaths) })
+  
+  output$usaplot <- renderPlotly({                              #Beginning of Plotly Renderings For United States.
+    statedata <- state_data()
+    statedata <- statedata %>% mutate(state_alt = setNames(state.abb,state.name)[state], 
+                                      date = as.numeric(as.Date(date))-18282,
+                                      #cases = ifelse(state == "New York"| state == "NY" | state == "NJ", 0, cases),
+                                      log_cases = ifelse(cases == 0,0, log(cases)),
+                                      log_deaths = ifelse(deaths == 0, 0 , log(deaths))
+                                      )
+    #statedata$hover <- with(statedata, paste(state, "<br>", "Total Cases:", cases, "<br>", "Logarithm Scale:", round(log_cases, digits = 3), "<br>", "Deaths:", deaths))
+    
+    l <- list(color = toRGB("white"), width = 2)
+    # specify some map projection/options
+    g <- list(
+      scope = 'usa',
+      projection = list(type = 'albers usa'),
+      showlakes = TRUE,
+      lakecolor = toRGB('white')
+    )
+    
+    #fig <- plot_geo(statedata, locationmode = "USA-states")
+    if(input$`deaths-cases` == "deaths"){
+      if(input$`log-normal` == "log"){
+        statedata$hover <- with(statedata, 
+                                paste(state, "<br>", 
+                                      "Total Cases:", cases, "<br>", 
+                                      "Logarithm Scale:", round(log_cases, digits = 3), 
+                                      "<br>", "Total Deaths:", deaths))
+        fig <- plot_geo(statedata, locationmode = "USA-states")
+        fig <- fig %>% add_trace(
+              z = ~log_deaths, text = ~hover, locations = ~state_alt, color = ~log_deaths, frame = ~date
+              ) %>% layout(geo = g)
+      } else { #deaths, normal representation
+          statedata$hover <- with(statedata, 
+                                paste(state, "<br>", 
+                                      "Total Cases:", cases, "<br>", 
+                                      #"Logarithm Scale:", round(log_cases, digits = 3), 
+                                      "<br>", "Total Deaths:", deaths))
+          fig <- plot_geo(statedata, locationmode = "USA-states")
+          fig <- fig %>% add_trace(
+            z = ~deaths, text = ~hover, locations = ~state_alt, color = ~deaths, frame = ~date
+          ) %>% layout(geo = g)
+      }
+    } else { # Display cases
+      if(input$`log-normal` == "log"){
+        statedata$hover <- with(statedata, 
+                                paste(state, "<br>", 
+                                      "Total Cases:", cases, "<br>", 
+                                      "Logarithm Scale:", round(log_cases, digits = 3), 
+                                      "<br>", "Total Deaths:", deaths))
+        fig <- plot_geo(statedata, locationmode = "USA-states")
+        fig <- fig %>% add_trace(
+          z = ~log_cases, text = ~hover, locations = ~state_alt, color = ~log_cases, frame = ~date
+        ) %>% layout(geo = g)
+      } else { # Display cases without a logarithmic scale
+        statedata$hover <- with(statedata, 
+                                paste(state, "<br>", 
+                                      "Total Cases:", cases, "<br>", 
+                                      #"Logarithm Scale:", round(log_cases, digits = 3), 
+                                      "<br>", "Total Deaths:", deaths))
+        fig <- plot_geo(statedata, locationmode = "USA-states")
+        fig <- fig %>% add_trace(
+          z = ~cases, text = ~hover, locations = ~state_alt, color = ~cases, frame = ~date
+        ) %>% layout(geo = g)
+      }
+    }
+    
+    return(fig)
+  })                                      #End of plotly Graphs for Total United STates
+  
+  output$county_slider <- renderUI({
+    data <- county_data() %>% filter(state == input$state) %>% mutate(date = as.Date(date))
+    smallest_date <- min(data$date)
+    #data <- data %>% mutate(date = date)
+    object <- sliderInput("date",
+                          label = "Select Days from Zero-Day",
+                          min = min(data$date, na.rm = TRUE),
+                          max = max(data$date, na.rm = TRUE),
+                          value = max(data$date, na.rm = TRUE),
+                          animate = TRUE)
   })
   
-  output$myplot <- renderPlot({
-    corona <- df()
-    corona.cases <- corona %>% count(County) %>% mutate(County = tolower(County))
-    corona.cases <- corona.cases %>% mutate(County = fct_recode(County, 
-                                                                `miami-dade` = "dade", 
-                                                                `st johns` = "st. johns", 
-                                                                `st lucie` = "st. lucie")) %>% 
-      filter(County != "unknown")
-    floridamapdata <- map_data("county") %>% filter(region == "florida")
-    covidFLmap <- left_join(floridamapdata, corona.cases, by = c(subregion = "County"))
+  
+  output$county <- renderPlotly({
+    corona <- county_data()
+    `%nin%` = Negate(`%in%`)
+    corona <- corona %>% #filter(state == input$state) %>% 
+      mutate(county = tolower(county), 
+             state = tolower(state),
+             ) %>% 
+      filter(state %nin% setdiff(unique(tolower(corona$state)), unique(map_data("county")$region)) )
+    corona <- corona %>% filter(date == "2020-04-09")
+    corona.cases <- corona %>% mutate(county = fct_recode(county, 
+                                                        `de kalb` = "dekalb",
+                                                        `dona ana` = "doña ana",
+                                                        `du page` = "dupage",
+                                                        `la porte` = "laporte",
+                                                        `la salle` = "lasalle",
+                                                        `new york` = "new york city",
+                                                        `obrien` = "o'brien",
+                                                        `prince georges` = "prince george's",
+                                                        `queen annes` = "queen anne's",
+                                                        `st bernard` = "st. bernard",
+                                                        `st charles` = "st. charles",
+                                                        `st clair` = "st. clair",
+                                                        `st francis` ="st. francis",
+                                                        `st francois` = "st. francois",
+                                                        `st helena` = "st. helena",
+                                                        `st james` = "st. james",
+                                                        `st john the baptist` = "st. john the baptist",
+                                                        `st johns` = "st. johns",
+                                                        `st joseph` = "st. joseph",
+                                                        `st landry` = "st. landry",
+                                                        `st lawrence` = "st. lawrence",
+                                                        `st louis` =  "st. louis",
+                                                        `st louis city` = "st. louis city",
+                                                        `st lucie` = "st. lucie",
+                                                        `st martin` = "st. martin",
+                                                        `st mary` = "st. mary",
+                                                        `st marys` = "st. mary's",
+                                                        `st tammany` = "st. tammany",
+                                                        `ste genevieve` = "ste. genevieve"),
+                                      log_cases = log(cases),
+                                      log_deaths = log(deaths)) %>% 
+      filter(county != "unknown" & state == tolower(input$state)) #%>% filter(date == Sys.Date()-1)
+    print(str(corona.cases$date))
+    #changing state mapping
+    mapping <- map_data("county") %>% filter(region == tolower(input$state))
     
+    mapitup <- left_join(mapping,corona.cases, by = c(subregion = "county") )
     
-    plot <- ggplot(covidFLmap, aes(x = long, y = lat, group = group, fill = n)) +
-      geom_polygon(color = "black", 
-                   size = 0.5) + 
-      theme_minimal() + 
-      scale_fill_viridis_c() + 
-      labs(title = "Coronavirus Cases in Florida, by County",
-           fill = "Number of Cases") + 
-      coord_map(projection = "albers", 
-                lat0 = 25, 
-                lat1 = 31)
-    return(plot)
+    if(input$`deaths-cases` == "deaths"){
+      if(input$`log-normal` == "log"){
+        thingy <- ggplot(mapitup, aes(x = long,
+                                      y = lat, 
+                                      group = group,
+                                      fill = log_deaths, 
+                                      text = paste0(subregion, "<br>", "Cases: ", cases, "<br>", "Deaths: ", deaths))) +
+          geom_polygon(color = "black", 
+                       size = 0.5) + 
+          theme_minimal() + 
+          scale_fill_viridis_c() + 
+          labs(title = paste("Coronavirus", input$`deaths-cases`, "in" ,input$state, "by County with", input$`log-normal`, "scale"),
+               fill = paste("Number of", input$`deaths-cases`)) + 
+          coord_map(projection = "albers", 
+                    lat0 = 25, 
+                    lat1 = 31)
+      }else{
+        thingy <- ggplot(mapitup, aes(x = long,
+                                      y = lat, 
+                                      group = group,
+                                      fill = deaths, 
+                                      text = paste0(subregion, "<br>", "Cases: ", cases, "<br>", "Deaths: ", deaths))) +
+          geom_polygon(color = "black", 
+                       size = 0.5) + 
+          theme_minimal() + 
+          scale_fill_viridis_c() + 
+          labs(title = paste("Coronavirus", input$`deaths-cases`, "in" ,input$state, "by County with", input$`log-normal`, "scale"),
+               fill = paste("Number of", input$`deaths-cases`)) + 
+          coord_map(projection = "albers", 
+                    lat0 = 25, 
+                    lat1 = 31)
+      }
+    }else{
+      if(input$`log-normal` == "log"){
+        thingy <- ggplot(mapitup, aes(x = long,
+                                      y = lat, 
+                                      group = group,
+                                      fill = log_cases, 
+                                      text = paste0(subregion, "<br>", "Cases: ", cases, "<br>", "Deaths: ", deaths))) +
+          geom_polygon(color = "black", 
+                       size = 0.5) + 
+          theme_minimal() + 
+          scale_fill_viridis_c() + 
+          labs(title = paste("Coronavirus", input$`deaths-cases`, "in" ,input$state, "by County with", input$`log-normal`, "scale"),
+               fill = paste("Number of", input$`deaths-cases`)) + 
+          coord_map(projection = "albers", 
+                    lat0 = 25, 
+                    lat1 = 31)
+      } else {
+        thingy <- ggplot(mapitup, aes(x = long,
+                                      y = lat, 
+                                      group = group,
+                                      fill = cases, 
+                                      text = paste0(subregion, "<br>", "Cases: ", cases, "<br>", "Deaths: ", deaths))) +
+          geom_polygon(color = "black", 
+                       size = 0.5) + 
+          theme_minimal() + 
+          scale_fill_viridis_c() + 
+          labs(title = paste("Coronavirus", input$`deaths-cases`, "in" ,input$state, "by County with", input$`log-normal`, "scale"),
+               fill = paste("Number of", input$`deaths-cases`)) + 
+          coord_map(projection = "albers", 
+                    lat0 = 25, 
+                    lat1 = 31)
+      }
+    }
+    return(ggplotly(thingy, tooltip = "text"))
   })
   
 
--- ui.R
+++ ui.R
@@ -2,31 +2,114 @@ library(shiny)
 library(tidyverse)
 library(shinydashboard)
 library(maps)
+library(plotly)
+library(DT)
 
-dashboardPage(
-  dashboardHeader(title = "My Dashboard"),
-  dashboardSidebar(
-    sliderInput("size", "Size of Points:", min=0.2, max=5, value=2)
-  ),
-  dashboardBody(
-    # Boxes need to be put in a row (or column)
-    fluidRow(
-      box(width=6, 
-          status="info", 
-          title="Myplot",
-          solidHeader = TRUE,
-          plotOutput("myplot")
-      ),
-      box(width=6, 
-          status="warning", 
-          title = "Data Frame",
-          solidHeader = TRUE, 
-          collapsible = TRUE, 
-          footer="Read Remotely from File",
-          tableOutput("mydata")
-      )
+header <- dashboardHeader(title = "United States Coronavirus")
+
+
+sidebar <- dashboardSidebar(
+    sidebarMenu( id = "tabs",
+    menuItem("United States (total)", 
+             tabName = "usa", 
+             icon = icon("th"),
+             badgeLabel = "new", badgeColor = "green"),
+    
+    menuItem("Regional Map",
+             tabName = "region",
+             icon =  icon("th"),
+             badgeLabel = "new",
+             badgeColor = "yellow"),
+    
+    menuItem("Modeling",                                    #Modeling Tab
+             tabName = "models",
+             icon = icon("th"),
+             badgeLabel = "new", badgeColor = "blue"),      #End of Modeling Tab
+    
+    conditionalPanel("input.tabs == 'region'",              #Placement of regional panel
+                     selectInput("state",
+                                 label = "Select State",
+                                 choices = c("Alabama", "Arizona","Arkansas", "California", "Colorado",
+                                             "Connecticut","Delaware", "Florida","Georgia","Idaho", 
+                                             "Illinois", "Indiana","Iowa", "Kansas", "Kentucky", "Louisiana", 
+                                             "Maine","Maryland", "Massachusetts","Michigan", "Minnesota","Mississippi", 
+                                             "Missouri", "Montana","Nebraska", "Nevada", "New Hampshire","New Jersey",
+                                             "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma",
+                                             "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", 
+                                             "Texas","Utah", "Vermont","Virginia", "Washington", "West Virginia",
+                                             "Wisconsin","Wyoming"),
+                                 selected = "Florida"
+                     )
     ),
-    ## Add some more info boxes
+    conditionalPanel("input.tabs == 'usa' | input.tabs == 'region'",                 #Buttons for Total United States
+                     radioButtons("deaths-cases", 
+                                  label = h4("Plot:"),
+                                  choices = list("Deaths" = "deaths", "Cases" = "cases"),
+                                  selected = "cases"),
+                     radioButtons("log-normal",
+                                  label = h4("Color Scale"),
+                                  choices = list("Logarithmic" = "log", "Linear" = "line"))
+                     
+                     ),                                      #End of Buttons for United States
+    conditionalPanel("input.tabs == 'region'",
+                     uiOutput("county_slider")
+                     )
+    )
+)
 
+body <- dashboardBody(
+  tabItems(
+  # Boxes need to be put in a row (or column)
+    tabItem(tabName = "usa",                              #Layout for Total USA map
+      fluidRow(
+        box(width=6, 
+            status="info", 
+            title="United States Coronavirus Spread",
+            solidHeader = TRUE,
+            plotlyOutput("usaplot")
+        ),
+        box(width=6, 
+            status="warning", 
+            title = "Raw Data",
+            solidHeader = TRUE, 
+            collapsible = FALSE, 
+            footer="Read Remotely from Github.com/nytimes/covid-19-data/",
+            DTOutput("usadata")
+        )
+      )                                                     #End layout for USA map
+      ## Add some more info boxes
+      
+    ),
+    
+    tabItem(tabName = "region",      #Regional plots and data
+      fluidRow(
+        box(width = 6, 
+            status = "info",
+            title = "Options",
+            solidHeader = TRUE,
+            collapsible = FALSE,
+            plotlyOutput("county")
+            )
+        ),
+      fluidRow(
+        box(width = 6, 
+            status = "warning",
+            title = "Options",
+            solidHeader = TRUE,
+            collapsible = FALSE,
+            h2("As of Right now, I cannot get the plot to dynamically update with the slider bar."),
+            h4("It probably has something to do with the fact that I can't call the value from the dynamically rendered date slider"),
+            h4("Notice that when you change the state, the date from first infection changes"),
+            h4("However, I can't filter the data based on that value (and hence a slow load time).")
+            #DTOutput("poopy")
+            )
+        )
+      ),                              #End Of Regional Plots and Data
+    tabItem(tabName = "models",       #Modeling Tab
+            h2("modeling Outputs")
+    )
   )
 )
+
+
+dashboardPage(header, sidebar, body)
